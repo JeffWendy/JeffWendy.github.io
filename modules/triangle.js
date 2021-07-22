5072fd7b69873efd40aa1581e3e2f7b4e0df5b59
JeffWendy <66837750+JeffWendy@users.noreply.github.com>
2021-07-22 19:52:37 +0800

    Add files via upload

--- modules/triangle.js
+++ modules/triangle.js
@@ -1,8 +1,19 @@
-const name = 'square';
+const name = 'triangle';
+
+function degToRad(degrees) {
+  return degrees * Math.PI / 180;
+};
 
 function draw(ctx, length, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, length, length);
+
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x + length, y);
+  let triHeight = (length/2) * Math.tan(degToRad(60));
+  ctx.lineTo(x + (length/2), y + triHeight);
+  ctx.lineTo(x, y);
+  ctx.fill();
 
   return {
     length: length,
@@ -14,7 +25,7 @@ function draw(ctx, length, x, y, color) {
 
 function reportArea(length, listId) {
   let listItem = document.createElement('li');
-  listItem.textContent = `${name} area is ${length * length}px squared.`
+  listItem.textContent = `${name} area is ${Math.round((Math.sqrt(3)/4)*(length * length))}px squared.`
 
   let list = document.getElementById(listId);
   list.appendChild(listItem);
@@ -22,7 +33,7 @@ function reportArea(length, listId) {
 
 function reportPerimeter(length, listId) {
   let listItem = document.createElement('li');
-  listItem.textContent = `${name} perimeter is ${length * 4}px.`
+  listItem.textContent = `${name} perimeter is ${length * 3}px.`
 
   let list = document.getElementById(listId);
   list.appendChild(listItem);
